826b95bdacc5cec217bf6626f5a79a425cbe5bb2
emiranivala <taitufam@gmail.com>
2025-04-18 17:43:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,5 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7828838025:AAGLyspmmYl2R3E62ez6iqrks4QA8JfcT8s"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "922270982 7446465090").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://stroupsharmarkechaffin:12baRXqV5dq0iREA@cluster0.d8egf.mongodb.net/?retryWrites=true&w=majority")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002294535138"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002458919549"))
-
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1001294535138"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1001458919549"))
