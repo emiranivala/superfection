80a1105e622ed0dd900e292b455c149316f83ca1
emiranivala <taitufam@gmail.com>
2025-04-18 17:35:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "24210243"))
 API_HASH = getenv("API_HASH", "509031fb3790b968e489f71d591ebce5")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "7828838025:AAGLyspmmYl2R3E62ez6iqrks4QA8JfcT8s")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "922270982 7446465090").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://stroupsharmarkechaffin:12baRXqV5dq0iREA@cluster0.d8egf.mongodb.net/?retryWrites=true&w=majority")
 
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002294535138"))
 PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002458919549"))
